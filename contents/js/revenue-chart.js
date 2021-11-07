72f0f46cb820c63fcfbc3cc2830424db751f8ee7
nabilmersni <nabilmersni123@gmail.com>
2021-11-07 16:28:38 +0100

    finishing the home and users-list from the admin-dash

--- contents/js/revenue-chart.js
+++ contents/js/revenue-chart.js
@@ -65,3 +65,71 @@ const myChart = new Chart(lineGraph, {
   },
   options: lineGrapOptions,
 });
+
+let lineGraph_orders = document.querySelector(".line-graph_orders");
+
+// Options
+const lineGrapOptions_orders = {
+  aspectRatio: 2.5,
+  scales: {
+    yAxes: [
+      {
+        gridLines: {
+          display: false,
+        },
+        ticks: {
+          beginAtZero: false,
+          padding: 12,
+        },
+      },
+    ],
+    xAxes: [
+      {
+        ticks: {
+          padding: 12,
+        },
+        gridLines: {
+          display: false,
+        },
+      },
+    ],
+  },
+  legend: {
+    display: false,
+  },
+};
+const myChart_orders = new Chart(lineGraph_orders, {
+  curvature: 1,
+  type: "bar",
+  data: {
+    labels: [
+      "Jan",
+      "Feb",
+      "Mar",
+      "Apr",
+      "May",
+      "June",
+      "jul",
+      "Aug",
+      "Sep",
+      "Oct",
+      "Nov",
+      "Dec",
+    ],
+    datasets: [
+      {
+        data: [10, 25, 9, 15, 30, 40, 60, 55, 70, 10, 20, 25],
+        borderColor: "rgba(255,117,92,1)",
+        pointBorderColor: "rgba(255,255,255,1)",
+        pointBackgroundColor: "rgba(255,255,255,1)",
+        borderWidth: 2,
+        borderRadius: 5,
+        borderSkipped: false,
+        radius: 50,
+        fill: true,
+        backgroundColor: "rgba(255,117,92,1)",
+      },
+    ],
+  },
+  options: lineGrapOptions_orders,
+});
